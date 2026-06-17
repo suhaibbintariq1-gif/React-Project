8a7bb31762e055301e29f6c363aff1c6570575c5
Suhaib bin tariq <suhaibbintariq1@gmail.com>
2026-06-17 20:58:45 +0500

    Tailwind Css Update

---
+++ BG changer/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div classname="w-full h-screen duration-200"
+    style={{backgroundColor: color}}>
+      
+      
+    </div>
+  )
+}
+
+export default App
