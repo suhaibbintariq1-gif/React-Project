444e84dd600efc207190aa146afd668997b51220
Suhaib bin tariq <suhaibbintariq1@gmail.com>
2026-06-29 21:42:57 +0500

    accordian

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Accordian from "./componenets/accordian";
+
+function App() {
+  return (
+    <div className="App">
+      <Accordian />
+    </div>
+  );
+}
+
+export default App;
