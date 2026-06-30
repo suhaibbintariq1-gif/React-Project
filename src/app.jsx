a50e8f6b744c81ae071a89c9992935bc58d62115
Suhaib bin tariq <suhaibbintariq1@gmail.com>
2026-06-30 23:09:43 +0500

    random color generator

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordian from "./componenets/accordian";
-
+import RandomColor from "./componenets/random-color";
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* Accordian component */}
+       <Accordian/>
+       <RandomColor/>
     </div>
   );
 }
--- src/componenets/accordian/index.jsx
+++ src/componenets/accordian/index.jsx
@@ -34,7 +34,7 @@ export default function Accordian() {
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
+            <div className="item" key={dataItem.id}>
               <div
                 onClick={
                   enableMultiSelection
