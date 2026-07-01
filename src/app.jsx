1eca1da896ffc714b3eb40740bdb4a5e57c9b7c4
Suhaib bin tariq <suhaibbintariq1@gmail.com>
2026-07-01 21:09:44 +0500

    star rating

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accordian from "./componenets/accordian";
 import RandomColor from "./componenets/random-color";
+import StarRating from "./componenets/star-rating";
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
        <Accordian/>
        <RandomColor/>
+        <StarRating/>
     </div>
   );
 }
